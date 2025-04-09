81bf94a830a28e7a83561914d283cf243e74161f
kod0751 <50727457+kod0751@users.noreply.github.com>
2025-04-09 16:49:31 +0900

    [style] 폰트 적용

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,15 +2,7 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {
-      colors: {
-        primary: '#FF763B',
-        gold: '#FFC607',
-        white: '#FFFFFF',
-        black: '#323232',
-        gray: '#7F7F7F',
-        lightgray: '#F8F9FA',
-      },
-    },
+    extend: {},
   },
+  plugins: [],
 };
