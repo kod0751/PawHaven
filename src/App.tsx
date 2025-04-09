3ee128e5a9583a295780d92131f35a272084b343
kod0751 <50727457+kod0751@users.noreply.github.com>
2025-04-09 21:38:21 +0900

    [feat] 기본 라우팅 설정 및 공통 헤더 레이아웃 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Header } from './widgets/Header';
+import HomePage from './pages/home/HomePage';
 
 function App() {
-  return <Header />;
+  return (
+    <BrowserRouter>
+      <Header />;
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
