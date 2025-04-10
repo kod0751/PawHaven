51970335f9d6e66cf0b5fda1c60c4aabe7418896
kod0751 <50727457+kod0751@users.noreply.github.com>
2025-04-10 16:26:05 +0900

    [feat] 공용 카드 컴포넌트 구현

---
+++ src/shared/ui/Card.tsx
@@ -0,0 +1,35 @@
+export default function Card() {
+  return (
+    <div className="w-60 font-['NanumSquareNeoBold'] flex flex-col gap-1">
+      <div>
+        <img
+          src="/img/exAnimal.png"
+          alt="동물"
+          className="w-full h-40 object-cover rounded-md"
+        />
+      </div>
+      <div className="flex justify-between">
+        <span className="font-[NanumSquareNeoHeavy] text-[1.2rem]">
+          한국고양이
+        </span>
+        <img src="./src/assets/icon.png" alt="북마크" />
+      </div>
+      <div className="flex justify-between items-center text-[0.9rem]">
+        <span>나이</span>
+        <p>1살</p>
+      </div>
+      <div className="flex justify-between items-center text-[0.9rem]">
+        <span>시도군</span>
+        <p>안산시</p>
+      </div>
+      <div className="flex justify-between items-center text-[0.9rem]">
+        <span>성별</span>
+        <p>남아</p>
+      </div>
+      <div className="flex justify-between items-center text-[0.9rem]">
+        <span>중성화</span>
+        <p>미완료</p>
+      </div>
+    </div>
+  );
+}
