41c1df818be13d3bfe803d3c6b67db0493f88dc6
kod0751 <50727457+kod0751@users.noreply.github.com>
2025-04-11 18:34:57 +0900

    [chore] axios, reactQuery 설정

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './styles/global.css';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { queryClient } from './shared/lib/reactQuery.ts';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </StrictMode>
 );
