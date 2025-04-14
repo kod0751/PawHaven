6c79226a978b80dc8d46d003ce88c7bee449b36b
kod0751 <50727457+kod0751@users.noreply.github.com>
2025-04-14 17:39:54 +0900

    [faet] 유기동물리스트 페이지

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Header } from './widgets/Header';
+
 import HomePage from './pages/home/HomePage';
+import AnimalListPage from './pages/animalList/AnimalListPage';
 
 function App() {
   return (
@@ -8,6 +10,7 @@ function App() {
       <Header />;
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/list" element={<AnimalListPage />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/animalList/AnimalListPage.tsx
@@ -0,0 +1,3 @@
+export default function AnimalListPage() {
+  return <div>AnimalListPage</div>;
+}
