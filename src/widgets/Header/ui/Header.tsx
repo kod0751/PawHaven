27f093695b6d65f8059a58f7a928d371fe0c729c
kod0751 <50727457+kod0751@users.noreply.github.com>
2025-04-18 15:42:47 +0900

    [faet] 헤더 경로연결 및 스타일 수정

--- src/widgets/Header/ui/Header.tsx
+++ src/widgets/Header/ui/Header.tsx
@@ -1,19 +1,35 @@
+import { Link, useLocation } from 'react-router-dom';
 import Container from '../../../shared/ui/ResponsiveContainer';
 
 export default function Header() {
+  const location = useLocation();
+  const currentPath = location.pathname;
+
   return (
     <Container className="flex justify-between items-center py-8">
-      <div className="flex font-['jalnan'] text-2xl">
-        발바닥
-        <img
-          src="/img/logo.png"
-          alt="logo"
-          className="w-10 h-3 -translate-y-1"
-        ></img>
-        구조대
-      </div>
+      <Link to="/">
+        <div className="flex font-['jalnan'] text-2xl">
+          발바닥
+          <img
+            src="/img/logo.png"
+            alt="logo"
+            className="w-10 h-3 -translate-y-1"
+          ></img>
+          구조대
+        </div>
+      </Link>
       <ul className="flex gap-8 font-['NanumSquareNeoExtraBold']">
-        <li>유기동물보기</li>
+        <Link to="/list">
+          <li
+            className={`relative transition-all ${
+              currentPath === '/list'
+                ? 'text-orange-500'
+                : 'hover:text-orange-400'
+            }`}
+          >
+            유기동물보기
+          </li>
+        </Link>
         <li>털친소</li>
         <li>주변 보호소 찾기</li>
         <li>나의 관심동물</li>
