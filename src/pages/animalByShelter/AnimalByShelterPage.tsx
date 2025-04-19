e5dcae4418b7c8b92765b7f65b48b66536a9299e
kod0751 <50727457+kod0751@users.noreply.github.com>
2025-04-19 19:35:19 +0900

    [feat] 주변 보호소 찾기 UI 구현

--- src/pages/animalByShelter/AnimalByShelterPage.tsx
+++ src/pages/animalByShelter/AnimalByShelterPage.tsx
@@ -1,9 +1,15 @@
+import { useFilteredAnimalList } from '../../features/animalList/model/useFilteredAnimalList';
+import AnimalList from '../../features/animalList/ui/AnimalList';
+import CategoryFilter from '../../features/animalList/ui/Category';
 import ShelterMap from '../../widgets/ShelterMap/ShelterMap';
 
 export default function AnimalByShelterPage() {
+  const { filters, setFilters, data, isLoading } = useFilteredAnimalList();
   return (
     <div>
       <ShelterMap />
+      <CategoryFilter filters={filters} onFilterChange={setFilters} />
+      <AnimalList animals={data} isLoading={isLoading} filters={filters} />
     </div>
   );
 }
