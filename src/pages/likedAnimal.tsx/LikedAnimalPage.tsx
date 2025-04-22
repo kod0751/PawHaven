f656d101469e4aff52f9ec9b7f7cd0eb92fbd34c
kod0751 <50727457+kod0751@users.noreply.github.com>
2025-04-22 15:52:38 +0900

    [faet] 나의 관심동물 페이지 구현

--- src/pages/likedAnimal.tsx/LikedAnimalPage.tsx
+++ src/pages/likedAnimal.tsx/LikedAnimalPage.tsx
@@ -1,3 +1,19 @@
+import { useEffect, useState } from 'react';
+import AnimalList from '../../features/animalList/ui/AnimalList';
+
 export default function LikedAnimalPage() {
-  return <div>LikedAnimalPage</div>;
+  const [bookmarkedData, setBookmarkedData] = useState([]);
+
+  useEffect(() => {
+    const bookmarks = JSON.parse(
+      localStorage.getItem('bookmarkedAnimals') || '[]'
+    );
+    setBookmarkedData(bookmarks);
+  }, []);
+
+  return (
+    <div>
+      <AnimalList animals={bookmarkedData} pageType="liked" />
+    </div>
+  );
 }
