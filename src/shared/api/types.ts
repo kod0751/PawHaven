18476a599f84740ca025d1ab865604f737cc2bbf
kod0751 <50727457+kod0751@users.noreply.github.com>
2025-04-23 22:25:08 +0900

    [faet] 유기동물 데이터 타입추가

--- src/shared/api/types.ts
+++ src/shared/api/types.ts
@@ -9,4 +9,14 @@ export interface AnimalData {
   SHTER_NM: string;
   STATE_NM: string;
   BDWGH_INFO: string;
+  PBLANC_IDNTFY_NO: string;
+  RECEPT_DE: string;
+  DISCVRY_PLC_INFO: string;
+  SFETR_INFO: string;
+  PBLANC_BEGIN_DE: string;
+  PBLANC_END_DE: string;
+  REFINE_ROADNM_ADDR: string;
+  SHTER_TELNO: string;
+  REFINE_WGS84_LAT: string;
+  REFINE_WGS84_LOGT: string;
 }
