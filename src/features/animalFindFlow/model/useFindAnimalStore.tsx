b7089891a05465ef0ed350b3608b396327f464a1
kod0751 <50727457+kod0751@users.noreply.github.com>
2025-04-25 22:24:00 +0900

    [feat] 털친소 기능을 위한 멀티 스텝 폼 Zustand  스토어 구현

---
+++ src/features/animalFindFlow/model/useFindAnimalStore.tsx
@@ -0,0 +1,33 @@
+import { create } from 'zustand';
+
+type AnimalData = {
+  kind: string;
+  gender: string;
+  weight: string;
+  color: string;
+};
+
+type FindAnimalStore = {
+  step: number;
+  animalData: AnimalData;
+  nextStep: () => void;
+  prevStep: () => void;
+  updateAnimalData: (key: keyof AnimalData, value: string) => void;
+  reset: () => void;
+};
+
+export const useFindPetStore = create<FindAnimalStore>((set) => ({
+  step: 0,
+  animalData: { kind: '', gender: '', weight: '', color: '' },
+  nextStep: () => set((state) => ({ step: state.step + 1 })),
+  prevStep: () => set((state) => ({ step: Math.max(state.step - 1, 0) })),
+  updateAnimalData: (key, value) =>
+    set((state) => ({
+      animalData: { ...state.animalData, [key]: value },
+    })),
+  reset: () =>
+    set(() => ({
+      step: 0,
+      animalData: { kind: '', gender: '', weight: '', color: '' },
+    })),
+}));
