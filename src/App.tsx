39bd577a98e7599fb14dd7198bb3fcd9ebc2d28c
kod0751 <50727457+kod0751@users.noreply.github.com>
2025-04-25 16:05:38 +0900

    [feat] 털친소 경로 추가

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import AnimalListPage from './pages/animalList/AnimalListPage';
 import AnimalByShelterPage from './pages/animalByShelter/AnimalByShelterPage';
 import LikedAnimalPage from './pages/likedAnimal/LikedAnimalPage';
 import PetDetailPage from './pages/petDetail/PetDetailPage';
+import AnimalFindPage from './pages/AnimalFind/AnimalFindPage';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/list" element={<AnimalListPage />} />
+        <Route path="/find" element={<AnimalFindPage />} />
         <Route path="/shelter" element={<AnimalByShelterPage />} />
         <Route path="/liked" element={<LikedAnimalPage />} />
         <Route path="/detail/:id" element={<PetDetailPage />} />
