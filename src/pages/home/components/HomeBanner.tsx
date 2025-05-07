9a72bca556831dab8672e76ee9518220367b7ddc
kod0751 <50727457+kod0751@users.noreply.github.com>
2025-05-07 18:33:41 +0900

    [style] 배너 컴포넌트 반응형 디자인 수정

--- src/pages/home/components/HomeBanner.tsx
+++ src/pages/home/components/HomeBanner.tsx
@@ -4,17 +4,17 @@ export default function HomeBanner() {
   return (
     <Container className="flex justify-center items-center gap-10 py-8 xl:justify-between">
       <div className="flex flex-col gap-1">
-        <span className="font-['NanumSquareNeoExtraBold'] text-5xl mb-4">
+        <span className="font-['NanumSquareNeoExtraBold'] text-3xl mb-4 lg:text-5xl">
           버려진 아이들과
         </span>
-        <span className="font-['NanumSquareNeoExtraBold'] text-5xl mb-4">
+        <span className="font-['NanumSquareNeoExtraBold'] text-3xl mb-4 lg:text-5xl">
           당신의 <span className="text-orange-500">운명적 만남</span>,
           찾고계신가요?
         </span>
-        <p className="font-['NanumSquareNeo'] text-xl">
+        <p className="font-['NanumSquareNeo'] text-lg lg:text-xl">
           지금 당신의 따듯한 마음을 기다리는 친구들이 있습니다.
         </p>
-        <p className="font-['NanumSquareNeo'] text-xl">
+        <p className="font-['NanumSquareNeo'] text-lg lg:text-xl">
           유기동물 입양으로 가족이 되어주세요.
         </p>
 
