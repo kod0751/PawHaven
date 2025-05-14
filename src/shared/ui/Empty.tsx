58cfe37fc1c67c2af77cfdd8ea65f48b91bdb9de
kod0751 <50727457+kod0751@users.noreply.github.com>
2025-05-14 18:44:59 +0900

    [stlye] 공통 Empty 컴포넌트 스타일 수정

--- src/shared/ui/Empty.tsx
+++ src/shared/ui/Empty.tsx
@@ -1,8 +1,8 @@
 export default function Empty() {
   return (
-    <div className="col-span-full text-center text-gray-500 my-30 font-['NanumSquareNeo']">
+    <div className="flex flex-col justify-center items-center col-span-full text-center text-gray-500 my-30 font-['NanumSquareNeo']">
       <img src="/img/banner.png" alt="배너" className="w-lg h-auto" />
-      조건에 맞는 친구들이 없습니다.
+      <div>조건에 맞는 친구들이 없습니다.</div>
     </div>
   );
 }
