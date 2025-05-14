e36c11a4a2fcf9abbecf94a60f625989af55726f
kod0751 <50727457+kod0751@users.noreply.github.com>
2025-05-14 16:30:54 +0900

    [feat]  공통 Empty 컴포넌트 생성

---
+++ src/shared/ui/Empty.tsx
@@ -0,0 +1,8 @@
+export default function Empty() {
+  return (
+    <div className="col-span-full text-center text-gray-500 my-30 font-['NanumSquareNeo']">
+      <img src="/img/banner.png" alt="배너" className="w-lg h-auto" />
+      조건에 맞는 친구들이 없습니다.
+    </div>
+  );
+}
