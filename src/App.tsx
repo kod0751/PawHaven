beda61ce1421171c0c375a739ca39ad4266266c9
kod0751 <50727457+kod0751@users.noreply.github.com>
2025-06-06 14:48:11 +0900

    [feat] 404페이지 헤더 숨기기

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
 import { Header } from './widgets/Header';
 
 import HomePage from './pages/home/HomePage';
@@ -10,10 +10,18 @@ import AnimalFindPage from './pages/AnimalFind/AnimalFindPage';
 import ScrollToTopButton from './shared/ui/ScrollToTopButton';
 import NotFoundPage from './pages/Error/404';
 
-function App() {
+function AppContent() {
+  const location = useLocation();
+
+  // 유효한 경로들을 정의
+  const validPaths = ['/', '/list', '/find', '/shelter', '/liked'];
+  const isValidPath =
+    validPaths.includes(location.pathname) ||
+    location.pathname.startsWith('/detail/');
+
   return (
-    <BrowserRouter>
-      <Header />
+    <>
+      {isValidPath && <Header />}
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/list" element={<AnimalListPage />} />
@@ -24,6 +32,14 @@ function App() {
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
       <ScrollToTopButton />
+    </>
+  );
+}
+
+function App() {
+  return (
+    <BrowserRouter>
+      <AppContent />
     </BrowserRouter>
   );
 }
