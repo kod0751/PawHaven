ac4c697f1a9c6052478ee61af4f7ba5be8569c51
kod0751 <50727457+kod0751@users.noreply.github.com>
2025-06-07 21:55:23 +0900

    [feat] 디테일 페이지 에러관리

--- src/shared/api/useGetDetail.ts
+++ src/shared/api/useGetDetail.ts
@@ -1,7 +1,21 @@
 import { useAnimalList } from '../../features/animalList/model/useAnimalList';
 
 export const useGetDetail = (id: string | undefined) => {
-  const { data: allData, isLoading } = useAnimalList();
+  const { data: allData, isLoading, error } = useAnimalList();
+
+  if (id) {
+    return {
+      data: null,
+      isLoading: false,
+      error: new Error('잘못된 접근입니다.'),
+    };
+  }
+
   const data = allData?.find((value) => value.ABDM_IDNTFY_NO === id);
-  return { data, isLoading };
+  const notFoundError =
+    !isLoading && allData && !data
+      ? new Error('해당 동물 정보를 찾을 수 없습니다.')
+      : null;
+
+  return { data, isLoading, error: error || notFoundError };
 };
